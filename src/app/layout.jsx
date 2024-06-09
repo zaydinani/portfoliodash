194374728bbc93458041b2fc05793ca097e863d1
Zayd inani <zaydinani@protonmail.com>
2024-06-09 02:04:50 +0100

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,25 +1,30 @@
 import { Poppins } from "next/font/google";
-import "../styles/App.scss"
-const poppins = Poppins({ 
+import "../styles/App.scss";
+const poppins = Poppins({
   subsets: ["latin"],
-  weight: '400', 
+  weight: "400",
 });
 
 export const metadata = {
   title: "Zayd Dashboard",
   description: "A dashboard to manage my portfolio",
-  
+  icons: [
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      url: "/favicon-32x32.png",
+    },
+  ],
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/x-icon" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/x-icon" href="/favicon-32x32.png" />
       </head>
-      <body className={poppins.className}>
-        {children}
-      </body>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
