470c1b1747de871f7ec41d608d7606ec068a4056
Zayd inani <zaydinani@protonmail.com>
2024-06-11 22:51:53 +0100

    fixed more bugs

--- src/app/addArticle/page.jsx
+++ src/app/addArticle/page.jsx
@@ -1,45 +1,22 @@
 "use client";
-import React from "react";
-import { useState, useEffect } from "react";
-//import FroalaEditorComponent from "react-froala-wysiwyg";
+
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-
 import dynamic from "next/dynamic";
-
-const FroalaEditorComponent = dynamic(() => import("react-froala-wysiwyg"), {
-  ssr: false,
-});
 import "../../styles/addArticle.scss";
 import "froala-editor/css/froala_style.min.css";
 import "froala-editor/css/froala_editor.pkgd.min.css";
-import "froala-editor/js/plugins/align.min.js";
-import "froala-editor/js/plugins/code_view.min.js";
-import "froala-editor/js/plugins/code_beautifier.min.js";
-import "froala-editor/js/plugins/image.min.js";
-import "froala-editor/js/plugins/lists.min.js";
-import "froala-editor/js/plugins/quote.min.js";
-import "froala-editor/js/plugins/link.min.js";
-import "froala-editor/js/plugins/table.min.js";
-import "froala-editor/js/plugins/video.min.js";
-import "froala-editor/js/plugins/emoticons.min.js";
-import "froala-editor/js/plugins/special_characters.min.js";
-import "froala-editor/js/plugins/paragraph_format.min.js";
-import "froala-editor/js/plugins/paragraph_style.min.js";
-import "froala-editor/js/plugins/colors.min.js";
-import "froala-editor/js/plugins/font_family.min.js";
-import "froala-editor/js/plugins/font_size.min.js";
-import "froala-editor/js/plugins/fullscreen.min.js";
-import "froala-editor/js/plugins/draggable.min.js";
-import "froala-editor/js/plugins/char_counter.min.js";
-import "froala-editor/js/plugins/url.min.js";
-import "froala-editor/js/plugins/quick_insert.min.js";
+
+// Dynamically import FroalaEditorComponent with SSR disabled
+const FroalaEditorComponent = dynamic(() => import("react-froala-wysiwyg"), {
+  ssr: false,
+});
 
 function AddArticle() {
   const router = useRouter();
-
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
-  const [articleBannerImage, setArticleBannerImage] = useState(null); // State to store the selected image
+  const [articleBannerImage, setArticleBannerImage] = useState(null);
 
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
@@ -76,8 +53,40 @@ function AddArticle() {
 
   const handleBannerImageChange = (e) => {
     const file = e.target.files[0];
-    setArticleBannerImage(file); // Store the entire file object
+    setArticleBannerImage(file);
   };
+
+  useEffect(() => {
+    const loadFroalaPlugins = async () => {
+      if (typeof window !== "undefined") {
+        await Promise.all([
+          import("froala-editor/js/plugins/align.min.js"),
+          import("froala-editor/js/plugins/code_view.min.js"),
+          import("froala-editor/js/plugins/code_beautifier.min.js"),
+          import("froala-editor/js/plugins/image.min.js"),
+          import("froala-editor/js/plugins/lists.min.js"),
+          import("froala-editor/js/plugins/quote.min.js"),
+          import("froala-editor/js/plugins/link.min.js"),
+          import("froala-editor/js/plugins/table.min.js"),
+          import("froala-editor/js/plugins/video.min.js"),
+          import("froala-editor/js/plugins/emoticons.min.js"),
+          import("froala-editor/js/plugins/special_characters.min.js"),
+          import("froala-editor/js/plugins/paragraph_format.min.js"),
+          import("froala-editor/js/plugins/paragraph_style.min.js"),
+          import("froala-editor/js/plugins/colors.min.js"),
+          import("froala-editor/js/plugins/font_family.min.js"),
+          import("froala-editor/js/plugins/font_size.min.js"),
+          import("froala-editor/js/plugins/fullscreen.min.js"),
+          import("froala-editor/js/plugins/draggable.min.js"),
+          import("froala-editor/js/plugins/char_counter.min.js"),
+          import("froala-editor/js/plugins/url.min.js"),
+          import("froala-editor/js/plugins/quick_insert.min.js"),
+        ]);
+      }
+    };
+
+    loadFroalaPlugins();
+  }, []);
 
   return (
     <main className="skill_container">
